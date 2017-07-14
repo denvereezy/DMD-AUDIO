e3f81bf89fb2291a9be0e40bb00766e2ba111d05
denver <denver@projectcodex.co>
2017-07-14 09:26:21 +0200

    server

---
+++ server.js
@@ -0,0 +1,48 @@
+const express          = require('express'),
+	  app              = express(),
+	  bodyParser 	   = require('body-parser'),
+	  cookieParser 	   = require('cookie-parser'),
+	  compression 	   = require('compression'),
+	  multer 		   = require('multer'),
+	  exhbs            = require('express-handlebars'),
+	  router		   = express.Router(),
+	  connection       = require('./routes/index'),
+	  music            = require('./routes/music'),
+	  MusicDataService = require('./data-services/musicDataService')
+      routing 		   = require('./routes/routing');
+
+
+const dbOptions = {
+	host      : 'localhost',
+    port      : 3306,
+    user      : 'admin',
+    password  : 'password',
+    database  : 'music'
+};
+
+const serviceSetupCallBack = function (connection) {
+	return {
+		musicDataService : new MusicDataService(connection)
+	}
+};
+
+app.use(connection(dbOptions, serviceSetupCallBack));
+app.use(express.static('public'));
+app.use(bodyParser.urlencoded({ extended : false }));
+app.use(bodyParser.json());
+app.use(compression());
+app.engine('handlebars', exhbs({defaultLayout : 'main'}));
+app.set('view engine', 'handlebars');
+
+app.get('/', routing.home);
+app.get('/playback', routing.playback);
+app.get('/library', routing.library);
+
+router.get('/music', music.songs);
+router.post('/music', multer({ dest: './public/uploads/'}).single('audio'), music.add);
+app.use('/api', router);
+
+const port = process.env.PORT || 8080;
+app.listen(port, () => {
+    console.log('App running on port', port);
+});
