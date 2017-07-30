ebe1da39239f09511ef8e4f6d3c75f4943036493
denver <denver@projectcodex.co>
2017-07-30 20:00:00 +0200

    updated

--- routes/routing.js
+++ routes/routing.js
@@ -1,15 +1,3 @@
-exports.library = function(req, res, next) {
-    res.render('library');
-};
-
-exports.playback = function(req, res, next) {
-    res.render('playback');
-};
-
 exports.home = function(req, res, next) {
-    res.render('playback');
-};
-
-exports.queue = function(req, res, next) {
-    res.render('queue');
+    res.render('index');
 };
