848dd44977a59af98aeba708e524c66e48b15e9c
denver <denver@projectcodex.co>
2017-08-13 20:00:00 +0200

    delete function

--- routes/music.js
+++ routes/music.js
@@ -1,4 +1,5 @@
 const co = require('co');
+const child_process = require('child_process');
 
 exports.add = function(req, res, next) {
     co(function * () {
@@ -64,3 +65,27 @@ exports.update = function(req, res, next) {
         }
     });
 };
+
+exports.delete = function(req, res, next) {
+    co(function*() {
+        try {
+            const services = yield req.getServices();
+            const musicDataService = services.musicDataService;
+            var id = req.params.id;
+            const music = yield musicDataService.edit(id);
+            var itemToDelete = music[0];
+            var command = 'rm public/' + itemToDelete.song;
+            child_process.exec(command, (error, stdout, stderr) => {
+                if (error) {
+                    console.log(`exec error: ${error}`);
+                    return;
+                }
+            });
+            musicDataService.delete(itemToDelete.id);
+            res.sendStatus(200);
+        } catch (err) {
+            next(err);
+            console.log(err);
+        }
+    });
+}
