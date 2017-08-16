f23b0e7fcd8d0c8ec50aedab728a74a322987fa8
denver <denver@projectcodex.co>
2017-08-16 20:00:00 +0200

    config

--- webpack.config.js
+++ webpack.config.js
@@ -5,8 +5,8 @@ module.exports = {
 
     entry: [
         __dirname + '/public/js/jquery-3.1.0.js',
-        __dirname + '/public/js/jquery.knob.js',
         __dirname + '/public/js/toastr.min.js',
+        __dirname + '/public/js/jquery.knob.js',
         __dirname + '/public/js/eq.js',
         __dirname + '/public/js/player.js'
     ],
