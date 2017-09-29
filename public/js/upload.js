624252d57d6e951db19fa8846e4eb3a6ed8c0b1f
denver <denver@projectcodex.co>
2017-09-29 13:22:04 +0200

    updated

--- public/js/upload.js
+++ public/js/upload.js
@@ -4,35 +4,24 @@ $(function(){
     var ul = $('#ul');
 
     $('#drop a').click(function(){
-        // Simulate a click on the file input button
-        // to show the file browser dialog
         $(this).parent().find('input').click();
     });
 
-    // Initialize the jQuery File Upload plugin
     $('#addForm').fileupload({
 
-        // This element will accept file drag/drop uploading
         dropZone: $('#drop'),
-
-        // This function is called when a file is added to the queue;
-        // either via the browse button, or via drag/drop:
         add: function (e, data) {
 
             var tpl = $('<li class="working"><input type="text" value="0" data-width="48" data-height="48"'+
                 ' data-fgColor="#0788a5" data-readOnly="1" data-bgColor="#3e4043" /><p></p><span></span></li>');
 
-            // Append the file name and file size
             tpl.find('p').text(data.files[0].name)
                          .append('<i>' + formatFileSize(data.files[0].size) + '</i>');
 
-            // Add the HTML to the UL element
             data.context = tpl.appendTo(ul);
 
-            // Initialize the knob plugin
             tpl.find('input').knob();
 
-            // Listen for clicks on the cancel icon
             tpl.find('span').click(function(){
 
                 if(tpl.hasClass('working')){
@@ -45,7 +34,6 @@ $(function(){
 
             });
 
-            // Automatically upload the file once it is added to the queue
             var jqXHR = data.submit();
             toastr.info('Song(s) added!', 'Success');
 
@@ -53,11 +41,8 @@ $(function(){
 
         progress: function(e, data){
 
-            // Calculate the completion percentage of the upload
             var progress = parseInt(data.loaded / data.total * 100, 10);
 
-            // Update the hidden input field and trigger a change
-            // so that the jQuery knob plugin knows to update the dial
             data.context.find('input').val(progress).change();
 
             if(progress == 100){
@@ -66,19 +51,16 @@ $(function(){
         },
 
         fail:function(e, data){
-            // Something has gone wrong!
             data.context.addClass('error');
+            toastr.warning('Upload failed', 'Something went wrong');
         }
 
     });
 
-
-    // Prevent the default action when a file is dropped on the window
     $(document).on('drop dragover', function (e) {
         e.preventDefault();
     });
 
-    // Helper function that formats the file sizes
     function formatFileSize(bytes) {
         if (typeof bytes !== 'number') {
             return '';
