ae07926b9de5a43f33e855645659b49e9ef9c6ab
denver <denver@projectcodex.co>
2017-11-01 15:24:39 +0200

    updated

--- sql/setup.sql
+++ sql/setup.sql
@@ -10,8 +10,6 @@ DROP TABLE IF EXISTS `music`;
 CREATE TABLE `music` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `song` varchar(100) DEFAULT NULL,
-  `name` varchar(100) DEFAULT NULL,
-  `createdAt` timestamp NULL DEFAULT NULL,
-  `updatedAt` timestamp NULL DEFAULT NULL,
+  `name` varchar(100) DEFAULT NULL
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=20 DEFAULT CHARSET=latin1;
